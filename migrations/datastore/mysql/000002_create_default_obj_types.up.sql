72bfc8243bcee265d5cc91d7ecb63779e8befc3a
Karan Kajla <kkajla12@gmail.com>
2023-03-21 14:20:04 -0700

    Add parent relation on default user object type

--- migrations/datastore/mysql/000002_create_default_obj_types.up.sql
+++ migrations/datastore/mysql/000002_create_default_obj_types.up.sql
@@ -5,7 +5,7 @@ VALUES
     ('role', '{"type": "role", "relations": {"member": {"inheritIf": "member", "ofType": "role", "withRelation": "member"}}}'),
     ('permission', '{"type": "permission", "relations": {"member": {"inheritIf": "anyOf", "rules": [{"inheritIf": "member", "ofType": "permission", "withRelation": "member"}, {"inheritIf": "member", "ofType": "role", "withRelation": "member"}]}}}'),
     ('tenant', '{"type": "tenant", "relations": {"admin": {}, "member": {"inheritIf": "manager"}, "manager": {"inheritIf": "admin"}}}'),
-    ('user', '{"type": "user", "relations": {}}'),
+    ('user', '{"type": "user", "relations": {"parent": {"inheritIf": "parent", "ofType": "user", "withRelation": "parent"}}}'),
     ('pricing-tier', '{"type": "pricing-tier", "relations": {"member": {"ofType": "pricing-tier", "inheritIf": "member", "withRelation": "member"}}}'),
     ('feature', '{"type": "feature", "relations": {"member": {"inheritIf": "anyOf", "rules": [{"inheritIf": "member", "ofType": "feature", "withRelation": "member"}, {"ofType": "pricing-tier", "inheritIf": "member", "withRelation": "member"}]}}}')
 ON DUPLICATE KEY UPDATE
