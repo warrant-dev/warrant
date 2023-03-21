8720b7c9bfe4a83ef9ee0f12b53e50521edb73cf
Karan Kajla <kkajla12@users.noreply.github.com>
2023-03-21 14:56:58 -0700

    Feat/add default built in obj types (#34)
    * Remove min length validation of ObjectTypeSpec.Relations to allow object types with no relations
    
    * Add mysql migration to create the built-in object types by default
    
    * Add postgres migration to create the built-in object types by default
    
    * Remove creation of default built-in object types from all tests
    
    * Add parent relation on default user object type
    
    * Add back min length validation of ObjectTypeSpec.Relations

---
+++ migrations/datastore/mysql/000002_create_default_obj_types.up.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+INSERT INTO objectType (typeId, definition)
+VALUES
+    ('role', '{"type": "role", "relations": {"member": {"inheritIf": "member", "ofType": "role", "withRelation": "member"}}}'),
+    ('permission', '{"type": "permission", "relations": {"member": {"inheritIf": "anyOf", "rules": [{"inheritIf": "member", "ofType": "permission", "withRelation": "member"}, {"inheritIf": "member", "ofType": "role", "withRelation": "member"}]}}}'),
+    ('tenant', '{"type": "tenant", "relations": {"admin": {}, "member": {"inheritIf": "manager"}, "manager": {"inheritIf": "admin"}}}'),
+    ('user', '{"type": "user", "relations": {"parent": {"inheritIf": "parent", "ofType": "user", "withRelation": "parent"}}}'),
+    ('pricing-tier', '{"type": "pricing-tier", "relations": {"member": {"ofType": "pricing-tier", "inheritIf": "member", "withRelation": "member"}}}'),
+    ('feature', '{"type": "feature", "relations": {"member": {"inheritIf": "anyOf", "rules": [{"inheritIf": "member", "ofType": "feature", "withRelation": "member"}, {"ofType": "pricing-tier", "inheritIf": "member", "withRelation": "member"}]}}}')
+ON DUPLICATE KEY UPDATE
+    definition = VALUES(definition),
+    deletedAt = NULL;
+
+COMMIT;
