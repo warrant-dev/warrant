12de410227f5f8f8259e9199a320a00647af0044
Karan Kajla <kkajla12@gmail.com>
2023-09-04 20:21:33 -0700

    Fix sqlite migration

--- migrations/datastore/sqlite/000006_add_metadata_to_object_table.up.sql
+++ migrations/datastore/sqlite/000006_add_metadata_to_object_table.up.sql
@@ -1,86 +1,96 @@
 ALTER TABLE object
-ADD COLUMN meta json DEFAULT NULL AFTER objectId;
+ADD COLUMN meta json DEFAULT NULL;
 
 -- backfill tenants
 UPDATE object
-INNER JOIN tenant ON object.objectId = tenant.tenantId
 SET meta = JSON_OBJECT("name", tenant.name)
+FROM tenant
 WHERE
+    object.objectId = tenant.tenantId AND
     object.objectType = "tenant" AND
     tenant.name IS NOT NULL;
 
 -- backfill users
 UPDATE object
-INNER JOIN user ON object.objectId = user.userId
 SET meta = JSON_OBJECT("email", user.email)
+FROM user
 WHERE
+    object.objectId = user.userId AND
     object.objectType = "user" AND
     user.email IS NOT NULL;
 
 -- backfill roles
 UPDATE object
-INNER JOIN role ON object.objectId = role.roleId
 SET meta = JSON_OBJECT("name", role.name)
+FROM role
 WHERE
+    object.objectId = role.roleId AND
     object.objectType = "role" AND
     role.name IS NOT NULL AND
     role.description IS NULL;
 
 UPDATE object
-INNER JOIN role ON object.objectId = role.roleId
 SET meta = JSON_OBJECT("name", role.name, "description", role.description)
+FROM role
 WHERE
+    object.objectId = role.roleId AND
     object.objectType = "role" AND
     role.name IS NOT NULL AND
     role.description IS NOT NULL;
 
 -- backfill permissions
 UPDATE object
-INNER JOIN permission ON object.objectId = permission.permissionId
 SET meta = JSON_OBJECT("name", permission.name)
+FROM permission
 WHERE
+    object.objectId = permission.permissionId AND
     object.objectType = "permission" AND
     permission.name IS NOT NULL AND
     permission.description IS NULL;
 
 UPDATE object
-INNER JOIN permission ON object.objectId = permission.permissionId
 SET meta = JSON_OBJECT("name", permission.name, "description", permission.description)
+FROM permission
 WHERE
+    object.objectId = permission.permissionId AND
     object.objectType = "permission" AND
     permission.name IS NOT NULL AND
     permission.description IS NOT NULL;
 
 -- backfill pricing-tiers
 UPDATE object
-INNER JOIN pricingTier ON object.objectId = pricingTier.pricingTierId
 SET meta = JSON_OBJECT("name", pricingTier.name)
+FROM pricingTier
 WHERE
+    object.objectId = pricingTier.pricingTierId AND
     object.objectType = "pricingTier" AND
     pricingTier.name IS NOT NULL AND
     pricingTier.description IS NULL;
 
 UPDATE object
-INNER JOIN pricingTier ON object.objectId = pricingTier.pricingTierId
 SET meta = JSON_OBJECT("name", pricingTier.name, "description", pricingTier.description)
+FROM pricingTier
 WHERE
+    object.objectId = pricingTier.pricingTierId AND
     object.objectType = "pricingTier" AND
     pricingTier.name IS NOT NULL AND
     pricingTier.description IS NOT NULL;
 
 -- backfill features
 UPDATE object
-INNER JOIN feature ON object.objectId = feature.featureId
 SET meta = JSON_OBJECT("name", feature.name)
+FROM feature
 WHERE
+    object.objectId = feature.featureId AND
     object.objectType = "feature" AND
     feature.name IS NOT NULL AND
     feature.description IS NULL;
 
 UPDATE object
-INNER JOIN feature ON object.objectId = feature.featureId
 SET meta = JSON_OBJECT("name", feature.name, "description", feature.description)
+FROM feature
 WHERE
+    object.objectId = feature.featureId AND
     object.objectType = "feature" AND
     feature.name IS NOT NULL AND
     feature.description IS NOT NULL;
